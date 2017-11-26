1b1d6f7b0884dc860ae500f518e56d34563c628b
Justin Tooley <jstooley823@gmail.com>
2017-11-26 23:55:16 +0000

    can add book by genres book list

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,16 @@ class GenresController < ApplicationController
   get 'genres/list' do
     erb :'genres/show'
   end
+
+  get 'genres/:id' do
+    
+    @genre = Genre.find_by(id: params[:id])
+
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+      erb :'genres/show_books'
+    else
+      erb :'genres/show_books'
+    end
+
 end
