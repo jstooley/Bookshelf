ad1e972dd203e8fb0a199c0ef9026534243f056d
Justin Tooley <jstooley823@gmail.com>
2017-11-26 18:52:35 +0000

    now saves the new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,12 @@ class BooksController < ApplicationController
   end
 
   post '/books/new' do
+  
+    @book = Book.create(title: params['title'], year_published: params['year_published'])
+    @genre = Genre.find_or_create_by(name: params['genre'])
+    @author = Author.find_or_create_by(name: params['author'])
+    @book.author = @author
+    @book.genre = @genre
     binding.pry
     redirect to '/show'
   end
