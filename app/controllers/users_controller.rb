cc98f18dea0bf15081e882a9cd62ad09c1648dd6
Justin Tooley <jstooley823@gmail.com>
2017-11-26 01:19:27 +0000

    added security to login sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,48 @@
 class UsersController < ApplicationController
 
   get '/login' do
-    erb :'users/login'
-  end
-
-  get '/signup' do
-    erb :'users/signup'
-  end
-
-  post 'login' do
 
     if logged_in?
       redirect to ''
     else
       erb :'users/login'
     end
-
+    
   end
 
-  post 'signup' do
+  get '/signup' do
 
     if logged_in?
       redirect to ""
     else
       erb :'users/signup'
+    end
+
+  end
+
+  post 'login' do
+
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/books'
+    else
+      redirect to "/login"
+    end
+
+  end
+
+  post 'signup' do
+
+    @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+    @user.save
+
+    if @user.save
+
+      session[:user_id] = @user.id
+      redirect to "/books"
+    else
+      redirect to "/signup"
     end
 
   end
