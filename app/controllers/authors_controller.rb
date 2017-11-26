80e7a1fe7cc60d8e92940ab9dc8572e47a2d83d1
Justin Tooley <jstooley823@gmail.com>
2017-11-26 23:28:48 +0000

    can add book from author book list

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,16 @@
 class AuthorsController < ApplicationController
 
   get '/authors/list' do
+      erb :'authors/show'
+  end
+
+  get '/authors/:id/show' do
+    @author = Author.find_by(id: params['id'])
     if logged_in?
       @user = User.find_by(id: session['user_id'])
-      erb :'authors/show'
+      erb :'authors/show_books'
     else
-      erb :'authors/show'
+      erb :'authors/show_books'
     end
   end
 
