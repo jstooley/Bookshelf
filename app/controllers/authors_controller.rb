4e387d55cfc58e6ceb2d1bc66d2b230ff1b079b0
Justin Tooley <jstooley823@gmail.com>
2017-12-01 17:30:16 +0000

    edited authors show view

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,18 +1,13 @@
 class AuthorsController < ApplicationController
 
   get '/authors' do
+      @authors = Author.all
       erb :'authors/index'
   end
 
 
   get '/authors/:id' do
-    @author = Author.find_by(id: params['id'])
-    if logged_in?
-      @user = User.find_by(id: session['user_id'])
-      erb :'authors/show'
-    else
-      erb :'authors/show'
-    end
+    @authors = Author.all
+    erb :'authors/show'
   end
-
 end
