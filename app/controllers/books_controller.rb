30441b441a950aae5d70729e2de416dce000d153
Justin Tooley <jstooley823@gmail.com>
2017-12-01 15:57:34 +0000

    one,last change

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -138,4 +138,5 @@ end
 # 2. Add the DELETE /books/:id/remove route with logic to remove a book from a user's book collection
 # 3. Clean up the DELETE /books/:id route to only handle deletion for verified user
 # 4. Implement the option to Add or Remove books (and delete edit for original posters) for your collection from the author books and genre books index pages
-# 5. User the current_user instance method for your controllers and views. 
+# 5. User the current_user instance method for your controllers and views.
+# 6. Make a pull request with the changes on a seperate github branch and @lukeghenco when submitting pull request. Do not merge into master branch until review is completed by Luke
