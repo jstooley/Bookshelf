24ea79f5e4d5b6e60b65bbd457b7fb524b725a06
Justin Tooley <jstooley823@gmail.com>
2017-12-02 19:37:47 +0000

    added logg in check to post request in book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,6 @@ class BooksController < ApplicationController
   end
 
   get '/books/:id/edit' do
-
     if logged_in?
       @book = Book.find_by(id: params['id'])
       erb :'books/edit'
@@ -24,83 +23,101 @@ class BooksController < ApplicationController
   end
 
   post '/books/new' do
-
-    @book = Book.create(title: params['title'], year_published: params['year_published'])
-    @genre = Genre.find_or_create_by(name: params['genre'])
-    @author = Author.find_or_create_by(name: params['author'])
-    @book.author = @author
-    @book.genre = @genre
-    @book.original_poster = current_user.id
-    @book.save
-    @author.new_book
-    UserBook.find_or_create_by(user_id: session['user_id'],book_id: @book.id)
-    redirect to '/show'
+    if logged_in?
+      @book = Book.create(title: params['title'], year_published: params['year_published'])
+      @genre = Genre.find_or_create_by(name: params['genre'])
+      @author = Author.find_or_create_by(name: params['author'])
+      @book.author = @author
+      @book.genre = @genre
+      @book.original_poster = current_user.id
+      @book.save
+      @author.new_book
+      UserBook.find_or_create_by(user_id: session['user_id'],book_id: @book.id)
+      redirect to '/show'
+    else
+      redirect to '/login'
+    end
   end
 
   post '/books/:id/add' do
-    @book = Book.find_by(id: params['id'])
-    UserBook.find_or_create_by(user_id: session['user_id'], book_id: params['id'])
-    redirect to '/show'
+    if logged_in?
+      @book = Book.find_by(id: params['id'])
+      UserBook.find_or_create_by(user_id: current_user, book_id: params['id'])
+      redirect to '/show'
+    else
+      redirect to '/login'
+    end
   end
 
   put '/books/:id/edit' do
-    @book = Book.find_by(id: params['id'])
-    @book.title = params['title']
-    @book.year_published = params['year_published']
-    @arthor_org = Author.find_by(id: @book.author_id)
-    @arthor_new = Author.find_or_create_by(name: params['author'])
-    unless @arthor_org == @arthor_new #check to see if changed arthor
-      @arthor_org.published_work -= 1
-      @arthor_org.save
-      @arthor_new.new_book
-      @book.author = @arthor_new
-      if @arthor_org.published_work == 0 #delete author if no longer has book
-        @arthir_org.delete
+    if logged_in?
+      @book = Book.find_by(id: params['id'])
+      @book.title = params['title']
+      @book.year_published = params['year_published']
+      @arthor_org = Author.find_by(id: @book.author_id)
+      @arthor_new = Author.find_or_create_by(name: params['author'])
+      unless @arthor_org == @arthor_new #check to see if changed arthor
+        @arthor_org.published_work -= 1
+        @arthor_org.save
+        @arthor_new.new_book
+        @book.author = @arthor_new
+        if @arthor_org.published_work == 0 #delete author if no longer has book
+          @arthir_org.delete
+        end
       end
+      @genre_org = Genre.find_by(id: @book.genre_id)
+      @genre_new = Genre.find_or_create_by(name: params['genre'])
+      unless @genre_org == @genre_new #check if genre changed
+        @book.genre = @genre_new
+      end
+      @book.save
+      if Book.find_by(genre_id: @genre_org.id) == nil #checks books to see if any have old genre if not deltes them
+        @genre_org.delete
+      end
+      redirect to '/show'
+    else
+      redirect to '/login'
     end
-    @genre_org = Genre.find_by(id: @book.genre_id)
-    @genre_new = Genre.find_or_create_by(name: params['genre'])
-    unless @genre_org == @genre_new #check if genre changed
-      @book.genre = @genre_new
-    end
-    @book.save
-    if Book.find_by(genre_id: @genre_org.id) == nil #checks books to see if any have old genre if not deltes them
-      @genre_org.delete
-    end
-    redirect to '/show'
   end
 
   delete '/books/:id/remove' do
-    UserBook.find_by(id: params[:id]).delete # if not op just take off list
+    if logged_in?
+      UserBook.find_by(id: params[:id]).delete # if not op just take off list
+    else
+      redirect to '/login'
+    end
   end
 
   delete '/books/:id' do
+    if logged_in?
+      @user_books = UserBook.find_by(id: params[:id])
+      @book = Book.find_by(id: @user_books.book_id)
+      @genre_count = 0# to see how many books have this genre
 
-    @user_books = UserBook.find_by(id: params[:id])
-    @book = Book.find_by(id: @user_books.book_id)
-    @genre_count = 0# to see how many books have this genre
-
-    UserBook.all.each do |user_book| #delete book off all list
-      if user_book.book_id == @book.id
-        user_book.delete
-      end
-    end
-
-    @genre_id = @book.genre.id #to have genre_id after book is deleted
-    @author_id = @book.author.id #to have genre_id after book is deleted
-    @book.delete # delete the book op is removing
-
-    if Book.all.include?(@genre_id)  # counts how many books have the genre of soon to be deleted book
-      Genre.find_by(id: @genre_id).delete #delete genre if the deleted book is the obnly one with it
-    end
-
-      @author = Author.find_by(id: @author_id)
-      @author.published_work -= 1
-      if @author.published_work == 0 # authors only book?
-        @author.delete # if so delete
+      UserBook.all.each do |user_book| #delete book off all list
+        if user_book.book_id == @book.id
+          user_book.delete
+        end
       end
 
-    redirect to '/show'
+      @genre_id = @book.genre.id #to have genre_id after book is deleted
+      @author_id = @book.author.id #to have genre_id after book is deleted
+      @book.delete # delete the book op is removing
+
+      if Book.all.include?(@genre_id)  # counts how many books have the genre of soon to be deleted book
+        Genre.find_by(id: @genre_id).delete #delete genre if the deleted book is the obnly one with it
+      end
+
+        @author = Author.find_by(id: @author_id)
+        @author.published_work -= 1
+        if @author.published_work == 0 # authors only book?
+          @author.delete # if so delete
+        end
+
+        redirect to '/show'
+      else
+        redirect to '/login'
+      end
   end
 end
 
