58bdeef352bd0038749d71c7b0ca6d3bd4200b55
Justin Tooley <jstooley823@gmail.com>
2017-12-02 20:39:12 +0000

    can look a individule genres without logging in

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,12 +6,8 @@ class GenresController < ApplicationController
   end
 
   get '/genres/:id' do
-    if logged_in?
       @genre = Genre.find_by(id: params['id'])
       erb :'genres/show'
-    else
-      redirect to '/login'
-    end
   end
 
 end
